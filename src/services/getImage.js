7ae5473de36bdbfd820697a3d86c9183b6058898
Freebo12 <Tumechko12@gmail.com>
2023-03-31 20:18:02 +0200

    add 12 images to find

--- src/services/getImage.js
+++ src/services/getImage.js
@@ -5,7 +5,7 @@ export const getImage = (searchText, page) => {
   const API_KEY = '33175218-249cfa9e0adbf794a9bfb1ae0';
 
   return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${searchText}&image_type=photo&per_page=3&page=${page}`
+    `${BASE_URL}?key=${API_KEY}&q=${searchText}&image_type=photo&per_page=12&page=${page}`
   );
 };
 
